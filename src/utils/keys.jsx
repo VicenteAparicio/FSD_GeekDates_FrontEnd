4c99841641a2510df714e00e358410f12562f666
Goodbyez <miradadesigual@gmail.com>
2021-09-07 17:26:42 +0200

    Try from aws front

---
+++ src/utils/keys.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const keysA = () => {
+
+let AWS_ACCESS_KEY_ID="AKIA33TBNO6SZXG6ZTXF";
+let AWS_SECRET_KEY="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";
+
+// [
+//     {
+//         "AllowedHeaders": [
+//             "*"
+//         ],
+//         "AllowedMethods": [
+//             "PUT",
+//             "HEAD",
+//             "GET"
+//         ],
+//         "AllowedOrigins": [
+//             "*"
+//         ],
+//         "ExposeHeaders": []
+//     }
+// ]
+
+}
+export default keysA;
